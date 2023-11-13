1aa5aec75e682995c324117937c971228b8b6d29
timmu <Tim.Mueller@edu.bib.de>
2023-11-13 19:48:30 +0000

    Main Activity First Layout Design Complete

--- app/src/main/java/com/example/albi/MainActivity.kt
+++ app/src/main/java/com/example/albi/MainActivity.kt
@@ -6,22 +6,36 @@ package com.example.albi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.albi.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,16 +48,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WelcomeLayout(){
-    Surface(color= Color(255, 190, 40), modifier = Modifier.fillMaxSize()) {
-        Column{
+    Surface(modifier = Modifier.fillMaxSize(),
+        color = FullMango) {
+        Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally){
             GlobeImage()
-            InfoButton()
             WelcomeText()
-            TextButton(
-                onClick = {  }
-            ) {
-                Text("Get Started!")
-            }
+            GetStartedButton()
+            InfoButton()
         }
     }
 }
@@ -62,12 +74,24 @@ fun InfoButton() {
             showDialog = showDialog.value,
             onDismiss = {showDialog.value = false})
     }
-    TextButton(
-        modifier = Modifier.padding(vertical = 24.dp),
-        onClick = {showDialog.value = true}
-    ) {
-        InfoImage()
+    Row(modifier = Modifier
+        .padding(16.dp)
+        .offset(y = 20.dp)
+        .background(
+            color = Color(255, 255, 255, 125),
+            shape = RoundedCornerShape(50.dp)
+        )
+    ){
+        TextButton(
+            onClick = {showDialog.value = true}
+        ) {
+            Text("about",
+                fontFamily = FontFamily(Font(R.font.rustycage)),
+                fontSize = 24.sp,
+                color = Fuji)
+        }
     }
+
 }
 
 @Composable
@@ -94,7 +118,39 @@ fun InfoAlert(msg : String, showDialog: Boolean, onDismiss: () -> Unit) {
 
 @Composable
 fun WelcomeText(){
-    Text("ALBI\na little bit interesting")
+    Text("A.L.B.I.",
+        fontFamily = FontFamily(Font(R.font.rustycage)),
+        fontSize = 64.sp)
+
+    Text("A Little Bit Interesting",
+        fontFamily = FontFamily(Font(R.font.rustycage)),
+        fontSize = 24.sp)
+}
+
+@Composable
+fun GetStartedButton(){
+    TextButton(modifier = Modifier.padding(top = 24.dp),
+        onClick = {  }
+    ) {
+        GetStartedButtonStyle()
+    }
+}
+
+@Composable
+fun GetStartedButtonStyle(){
+    Row(modifier = Modifier
+        .padding(16.dp)
+        .background(
+            color = Color(255, 255, 255, 125),
+            shape = RoundedCornerShape(50.dp)
+        )
+    ){
+        Text("Get started!",
+            fontFamily = FontFamily(Font(R.font.rustycage)),
+            fontSize = 32.sp,
+            color = Fuji,
+            modifier = Modifier.padding(20.dp))
+    }
 }
 
 
