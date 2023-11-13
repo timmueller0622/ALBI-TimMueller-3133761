c8fa6b2fd03133edc458adbce64f24319339bb2a
timmu <Tim.Mueller@edu.bib.de>
2023-11-13 17:50:54 +0000

    Main Activity Outline

---
+++ app/src/main/java/com/example/albi/GatherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.albi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class GatherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gather)
+    }
+}
--- app/src/main/java/com/example/albi/MainActivity.kt
+++ app/src/main/java/com/example/albi/MainActivity.kt
@@ -1,46 +1,100 @@
 package com.example.albi
 
+//Name: Tim Müller
+//St. Number: 3133761
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.albi.ui.theme.ALBITheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ALBITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            WelcomeLayout()
+        }
+    }
+}
+
+@Composable
+fun WelcomeLayout(){
+    Surface(color= Color(255, 190, 40), modifier = Modifier.fillMaxSize()) {
+        Column{
+            GlobeImage()
+            InfoButton()
+            WelcomeText()
+            TextButton(
+                onClick = {  }
+            ) {
+                Text("Get Started!")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GlobeImage(){
+    val image = painterResource(R.drawable.globeartworkmedium)
+    Image(painter = image, contentDescription = null)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ALBITheme {
-        Greeting("Android")
+fun InfoButton() {
+    val showDialog = remember { mutableStateOf(false) }
+    if (showDialog.value) {
+        InfoAlert(msg = stringResource(R.string.app_description),
+            showDialog = showDialog.value,
+            onDismiss = {showDialog.value = false})
+    }
+    TextButton(
+        modifier = Modifier.padding(vertical = 24.dp),
+        onClick = {showDialog.value = true}
+    ) {
+        InfoImage()
     }
 }
+
+@Composable
+fun InfoImage(){
+    val image = painterResource(R.drawable.infoiconverysmall)
+    Image(painter = image, contentDescription = null)
+}
+
+@Composable
+fun InfoAlert(msg : String, showDialog: Boolean, onDismiss: () -> Unit) {
+    if (showDialog) {
+        AlertDialog(
+            title = { Text(msg) },
+            onDismissRequest = onDismiss,
+            confirmButton = {
+                TextButton(onClick = onDismiss ) {
+                    Text("Dismiss")
+                }
+            },
+            dismissButton = {}
+        )
+    }
+}
+
+@Composable
+fun WelcomeText(){
+    Text("ALBI\na little bit interesting")
+}
+
+
